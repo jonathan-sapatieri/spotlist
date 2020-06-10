b5c823ce15957c65d39151d87caa055b07d2635a
Jonathan Sapatieri <contato@jonathansapatieri.com>
2020-06-09 21:18:17 -0300

    Updates

---
+++ precache-manifest.02153e0708bb379e057844d7392c8a70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127222c82dff453a005f2437472ce865",
+    "url": "/spotlist/index.html"
+  },
+  {
+    "revision": "b94df32422513dd73dbd",
+    "url": "/spotlist/static/css/main.966af1b9.chunk.css"
+  },
+  {
+    "revision": "5fcf23a0f9463a8a7f1d",
+    "url": "/spotlist/static/js/2.da91d7ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spotlist/static/js/2.da91d7ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94df32422513dd73dbd",
+    "url": "/spotlist/static/js/main.88b5b1cb.chunk.js"
+  },
+  {
+    "revision": "f7329ffdb2c09a152a71",
+    "url": "/spotlist/static/js/runtime-main.a3e83c88.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/spotlist/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
